5677523e8fca1de7464d71e5829f06ebc6066bcf
Vyacheslav Pukhanov <vyacheslav.pukhanov@gmail.com>
2018-12-29 20:35:50 +0300

    adds bucket creation on first run

--- main.go
+++ main.go
@@ -1,7 +1,29 @@
 package main
 
-import "github.com/vpukhanov/task/cmd"
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+
+	homedir "github.com/mitchellh/go-homedir"
+	"github.com/vpukhanov/task/cmd"
+	"github.com/vpukhanov/task/db"
+)
 
 func main() {
-	cmd.RootCmd.Execute()
+	home, err := homedir.Dir()
+	if err != nil {
+		panic(err)
+	}
+
+	dbPath := filepath.Join(home, ".tasks.db")
+	must(db.Init(dbPath))
+	must(cmd.RootCmd.Execute())
+}
+
+func must(err error) {
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
 }
