147055c431f73f35aad59b805379fd6ebdecea61
Vyacheslav Pukhanov <vyacheslav.pukhanov@gmail.com>
2018-12-29 21:17:32 +0300

    finalizes 'do' command

--- cmd/do.go
+++ cmd/do.go
@@ -2,9 +2,11 @@ package cmd
 
 import (
 	"fmt"
+	"os"
 	"strconv"
 
 	"github.com/spf13/cobra"
+	"github.com/vpukhanov/task/db"
 )
 
 // doCmd represents the do command
@@ -13,15 +15,28 @@ var doCmd = &cobra.Command{
 	Short: "Marks the task as complete",
 	Run: func(cmd *cobra.Command, args []string) {
 		var ids []int
+		tasks, err := db.AllTasks()
+		if err != nil {
+			fmt.Println("Something went wrong:", err.Error())
+			os.Exit(1)
+		}
 		for _, arg := range args {
 			id, err := strconv.Atoi(arg)
-			if err != nil {
+			if err != nil || id <= 0 || id > len(tasks) {
 				fmt.Println("invalid task id:", arg)
 			} else {
 				ids = append(ids, id)
 			}
 		}
-		fmt.Println(ids)
+		for _, id := range ids {
+			task := tasks[id-1]
+			err := db.DeleteTask(task.Key)
+			if err != nil {
+				fmt.Printf("Failed to mark \"%s\" as completed, error: %s\n", task.Value, err.Error())
+			} else {
+				fmt.Printf("Marked \"%s\" as completed\n", task.Value)
+			}
+		}
 	},
 }
 
